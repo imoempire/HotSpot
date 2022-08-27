28dc74084e59bc45f5120502f95f90c3f9eda85a
imoempire <mrimoempire@gmail.com>
2022-08-27 17:02:10 +0000

    modify

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,58 +1,81 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import { itemList } from "../Utils/data";
 import "./styles.css";
 
 const Header = () => {
   const form = useRef();
-  const sendEmail = (e) => {
+  const [filteredList, setFilteredList] = useState("");
+
+  const [area, setArea] = useState("");
+
+  const handleSearch = (e) => {
     e.preventDefault();
-    alert("Done");
+    const result = itemList.filter(function (freelancer) {
+      return freelancer.city === area;
+    });
+    setFilteredList(result);
   };
+
+  console.log(filteredList);
 
   return (
     <div className="container">
       <div className="header">
-
-      <div className="headerText">
-        <div className="headerInner">
-        <div>
-          <span className="stroke-Text">Best </span>
-        </div>
-        <div>
-          <span>Spots</span>
-        </div>
-        <div>
-          <span>around you</span>
-        </div>
-        </div>
-      </div>
-
-      <div className="forms">
-        <form ref={form} className="contact__form" onSubmit={sendEmail}>
-          <div className="contact__form-group">
-            <div className="contact__form-div">
-              <input
-                type="text"
-                name="Your City"
-                className="contact__form-input"
-                placeholder="Your City"
-              />
-            </div>
-            <div className="contact__form-div">
-              <input
-                type="text"
-                name="area"
-                className="contact__form-input"
-                placeholder="Your area"
-              />
+        <div className="inners">
+          {/* Text */}
+          <div className="headerText">
+            <div className="headerInner">
+              <div>
+                <span className="stroke-Text">Best </span>
+              </div>
+              <div>
+                <span>Spots around</span>
+              </div>
+              <div>
+                <span> you</span>
+              </div>
             </div>
           </div>
-          <button type="submit" className="btns">
-            Search
-          </button>
-        </form>
-      </div>
 
-    </div>
+          {/* Searching */}
+          <div className="forms">
+            <form ref={form} className="contact__form" onSubmit={handleSearch}>
+              <div className="contact__form-group">
+                <div className="contact__form-div">
+                  <input
+                    type="text"
+                    name="area"
+                    onChange={(e) => setArea(e.target.value)}
+                    value={area}
+                    className="contact__form-input"
+                    placeholder="Your area"
+                  />
+                </div>
+              </div>
+              <button type="submit" className="btns">
+                Search
+              </button>
+            </form>
+          </div>
+          {/* end */}
+        </div>
+
+        <div>
+          <h1 style={{ color: "white" }}>Your search result</h1>
+          {filteredList.length === 0 ? (
+            <div>empty</div>
+          ) : (
+            <div className="searchResult grid">
+              {filteredList.map((item) => (
+                <div className="searchCard grid" key={item.id}>
+                  <h1>{item.region}</h1>
+                  <h1>{item.city}</h1>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
--- src/Utils/data.js
+++ src/Utils/data.js
@@ -1,65 +1,99 @@
 export const data = [
   {
     name: "Melcom",
-    rates: '1.0',
+    rates: "1.0",
     location: "Madina, Accra",
-    id: 276
+    id: 276,
   },
   {
-   name: "Oyarifa Mall",
-   rates: '5.0',
-   location: "Madina, Accra",
-   id: 2
- },
- {
-   name: "Melcom",
-   rates: '5.0',
-   location: "Madina, Accra",
-   id: 4
- },
- {
-   name: "Melcom",
-   rates: '4.0',
-   location: "Madina, Accra",
-   id: 23
- },
+    name: "Oyarifa Mall",
+    rates: "5.0",
+    location: "Madina, Accra",
+    id: 2,
+  },
+  {
+    name: "Melcom",
+    rates: "5.0",
+    location: "Madina, Accra",
+    id: 4,
+  },
+  {
+    name: "Melcom",
+    rates: "4.0",
+    location: "Madina, Accra",
+    id: 23,
+  },
 ];
-
 
 export const locations = [
   {
     name: "Location 1",
-    location: { 
+    location: {
       lat: 41.3954,
-      lng: 2.162 
+      lng: 2.162,
     },
   },
   {
     name: "Location 2",
-    location: { 
+    location: {
       lat: 41.3917,
-      lng: 2.1649
+      lng: 2.1649,
     },
   },
   {
     name: "Location 3",
-    location: { 
+    location: {
       lat: 41.3773,
-      lng: 2.1585
+      lng: 2.1585,
     },
   },
   {
     name: "Location 4",
-    location: { 
+    location: {
       lat: 41.3797,
-      lng: 2.1682
+      lng: 2.1682,
     },
   },
   {
     name: "Location 5",
-    location: { 
+    location: {
       lat: 41.4055,
-      lng: 2.1915
+      lng: 2.1915,
     },
-  }
+  },
 ];
+
+export const itemList = [
+  { id: 1, region: "GREATER ACCRA" ,city: "Accra" },
+  { id: 2, region: "ASHANTI" ,city: "Kumasi" },
+  { id: 3, region: "WESTERN" ,city: "SEKONDI-TAKORADI" },
+  { id: 4, region: "WESTERN NORTH" ,city: "SEFWI WIASO" },
+  { id: 5, region: "BRONG AHAFO" ,city: "SUNYANI" },
+  { id: 6, region: "VOLTA" ,city: "Ho" },
+  { id: 7, region: "AHAFO" ,city: "GOASO" },
+  { id: 8, region: "BONO EAST" ,city: "TECHIMAN" },
+  { id: 9, region: "EASTERN" ,city: "KOFORIDUA" },
+  { id: 10, region: "UPPER EAST" ,city: "BOLGATANGA" },
+  { id: 11, region: "SAVANNAH" ,city: "DAMONGO" },
+  { id: 12, region: "UPPER WEST" ,city: "WA" },
+  { id: 13, region: "OTI" ,city: "DAMBAI" },
+  { id: 14, region: "CENTRAL" ,city: "CAPE COAST" },
+  { id: 15, region: "NORTH EAST" ,city: "NALERIGU" },
+  { id: 16, region: "NORTHERN" ,city: "TAMALE" },
+];
+
+// AHAFO	
+// ASHANTI	
+// BONO EAST	
+// BRONG AHAFO	
+// CENTRAL	
+// EASTERN	
+// NORTH EAST	
+// NORTHERN	
+// OTI	
+// SAVANNAH	
+// UPPER EAST	
+// UPPER WEST	
+// WESTERN	
+// WESTERN NORTH	
+// VOLTA	HO
